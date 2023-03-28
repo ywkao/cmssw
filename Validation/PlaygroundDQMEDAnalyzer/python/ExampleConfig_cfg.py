0af495e8fd11d10d8b60ebb8eb32925cb282e683
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2023-03-28 13:16:02 +0200

    Add a playground DQMEDAnalyzer module

---
+++ Validation/PlaygroundDQMEDAnalyzer/python/ExampleConfig_cfg.py
@@ -0,0 +1,45 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents.input = 1
+process.source = cms.Source('EmptySource')
+
+#--------------------------------------------------
+# ped subtraction
+#--------------------------------------------------
+#process.load("Validation.PlaygroundDQMEDAnalyzer.playgrounddqmedanalyzer_cfi")
+process.playgrounddqmedanalyzer = cms.EDProducer('PlaygroundDQMEDAnalyzer',
+  folder = cms.string('HGCAL/RecHits'),
+  DataType = cms.string('beam'),
+  CalibrationFlags = cms.vint32( 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+  mightGet = cms.optional.untracked.vstring
+)
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.load("DQMServices.FileIO.DQMFileSaverOnline_cfi")
+process.dqmSaver.tag = 'TEST'
+process.dqmSaver.path = './eos/'
+
+process.p1 = cms.Path(process.playgrounddqmedanalyzer + process.dqmSaver)
+
+
+#--------------------------------------------------
+# CM subtraction
+#--------------------------------------------------
+process.playgrounddqmedanalyzer_cm = cms.EDProducer('PlaygroundDQMEDAnalyzer',
+  folder = cms.string('HGCAL/RecHits'),
+  DataType = cms.string('beam'),
+  CalibrationFlags = cms.vint32( 1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+  mightGet = cms.optional.untracked.vstring
+)
+
+process.dqmSaver_cm = process.dqmSaver.clone(
+  tag = 'CMTEST',
+  path = './eos/'
+)
+
+process.p2 = cms.Path(process.playgrounddqmedanalyzer_cm + process.dqmSaver_cm)
