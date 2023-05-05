a64867a887b839f710ca09952614601f11cad386
yulunmiao <yulunmiao@gmail.com>
2023-05-05 16:21:04 +0200

    update to use file on eos

--- RecoLocalCalo/HGCalRecAlgos/test/testHGCalDigiToRecHit.py
+++ RecoLocalCalo/HGCalRecAlgos/test/testHGCalDigiToRecHit.py
@@ -4,16 +4,21 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 process = cms.Process("testRecHit")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:test.root')
+    fileNames = cms.untracked.vstring('file:/eos/user/y/yumiao/public/HGCALRawDataHandling/Digis/testFakeDigis.root')
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
-process.load("RecoLocalCalo.HGCalRecAlgos.hgcalRecHits_cfi")
 
 process.output = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string("./testRecHit.root"),
 )
+
 process.outpath = cms.EndPath(process.output)
+
+process.hgcalRecHits = cms.EDProducer('HGCalDigiToRecHit',
+  Digis = cms.InputTag('hgcalDigis', 'DIGI', 'TEST'),
+)
+
 process.p = cms.Path(process.hgcalRecHits)
 
