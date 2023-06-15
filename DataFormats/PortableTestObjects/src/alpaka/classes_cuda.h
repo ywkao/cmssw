3ff56235bcff1c3ed954ff7957a9bbbc7029ff8a
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2023-06-15 15:45:40 +0200

    Clean up unused files introduced from 4f322e9843f048d163fbcbadbe238eee8cad5e6e

--- DataFormats/PortableTestObjects/src/alpaka/classes_cuda.h
+++ DataFormats/PortableTestObjects/src/alpaka/classes_cuda.h
@@ -2,5 +2,3 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/PortableTestObjects/interface/TestSoA.h"
 #include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceCollection.h"
-#include "DataFormats/PortableTestObjects/interface/TestRecHitSoA.h"
-#include "DataFormats/PortableTestObjects/interface/alpaka/TestDeviceRecHitCollection.h"
--- DataFormats/PortableTestObjects/src/classes.h
+++ DataFormats/PortableTestObjects/src/classes.h
@@ -1,5 +1,3 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/PortableTestObjects/interface/TestHostCollection.h"
 #include "DataFormats/PortableTestObjects/interface/TestSoA.h"
-#include "DataFormats/PortableTestObjects/interface/TestHostRecHitCollection.h"
-#include "DataFormats/PortableTestObjects/interface/TestRecHitSoA.h"
