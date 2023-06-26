b1dcc5657df4ec68cc399ebfd13a5e87b21af64c
maria <dalfonso@cern.ch>
2023-06-26 13:52:41 +0200

    add extra informations for Si rechits

--- DPGAnalysis/HGCalNanoAOD/interface/HitInfoTableProducer.h
+++ DPGAnalysis/HGCalNanoAOD/interface/HitInfoTableProducer.h
@@ -24,12 +24,32 @@ class HitInfoTableProducer : public edm::stream::EDProducer<> {
 
   ~HitInfoTableProducer() override {}
 
-  unsigned int virtual layer(const typename T::value_type& hit) { 
-    throw cms::Exception("HitInfoTableProducer:layer") << "Virtual function layer is not implemented!"; 
+  unsigned int virtual layer(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:layer") << "Virtual function layer is not implemented!";
   }
 
-  std::pair<int, int> virtual uv(const typename T::value_type& hit) { 
-    throw cms::Exception("HitInfoTableProducer:uv") << "Virtual function uv coordinates is not implemented!"; 
+  unsigned int virtual siThickIndex(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:siThickIndex") << "Virtual function siThickIndex is not implemented!";
+  }
+
+  unsigned int virtual zside(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:zside") << "Virtual function zside is not implemented!";
+  }
+
+  float virtual eta(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:eta") << "Virtual function eta is not implemented!";
+  }
+
+  float virtual phi(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:phi") << "Virtual function phi is not implemented!";
+  }
+
+  std::pair<int, int> virtual uv(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:uv") << "Virtual function uv coordinates is not implemented!";
+  }
+
+  std::pair<int, int> virtual wafer(const typename T::value_type& hit) {
+    throw cms::Exception("HitInfoTableProducer:wafer") << "Virtual function wafer coordinates is not implemented!";
   }
 
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override {
@@ -38,21 +58,39 @@ class HitInfoTableProducer : public edm::stream::EDProducer<> {
     iEvent.getByToken(src_, objs);
 
     std::vector<unsigned int> layervals;
+    std::vector<int> sithickvals;
+    std::vector<int> zsidevals;
+    std::vector<float> etavals;
+    std::vector<float> phivals;
     std::vector<int> uvals;
     std::vector<int> vvals;
+    std::vector<int> waferuvals;
+    std::vector<int> wafervvals;
 
     for (const auto& obj : *objs) {
       if (cut_(obj)) {
+	sithickvals.emplace_back(siThickIndex(obj));
 	layervals.emplace_back(layer(obj));
+	zsidevals.emplace_back(zside(obj));
+	etavals.emplace_back(eta(obj));
+	phivals.emplace_back(phi(obj));
 	uvals.emplace_back(uv(obj).first);
 	vvals.emplace_back(uv(obj).second);
+	waferuvals.emplace_back(wafer(obj).first);
+	wafervvals.emplace_back(wafer(obj).second);
       }
     }
 
     auto tab = std::make_unique<nanoaod::FlatTable>(layervals.size(), name_, false, true);
-    tab->addColumn<int>("u", uvals, "u cell coordinate");
-    tab->addColumn<int>("v", vvals, "v cell coordinate");
+    tab->addColumn<int>("u_wafer", uvals, "u wafer coordinate");
+    tab->addColumn<int>("v_wafer", vvals, "v wafer coordinate");
+    tab->addColumn<int>("u_cell", uvals, "u cell coordinate");
+    tab->addColumn<int>("v_cell", vvals, "v cell coordinate");
     tab->addColumn<unsigned int>("layer", layervals, "layer coordinate");
+    tab->addColumn<float>("eta", etavals, "eta coordinate");
+    tab->addColumn<float>("phi", phivals, "phi coordinate");
+    tab->addColumn<int>("zside", zsidevals, "z side coordinate");
+    tab->addColumn<int>("siThickIndex", sithickvals, "SiThickIndex");
 
     iEvent.put(std::move(tab));
 
