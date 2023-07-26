4da2d3bf58a32078a3b17329d50a5b4fd1607bdc
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2023-07-26 12:06:54 +0200

    [Calibration] Add SoA and portable collections for calibration parameters & declare it to the framework

---
+++ RecoLocalCalo/HGCalRecAlgos/interface/alpaka/HGCalCalibrationParameterDeviceCollection.h
@@ -0,0 +1,21 @@
+#ifndef RecoLocalCalo_HGCalRecAlgos_interface_alpaka_HGCalCalibrationParameterDeviceCollection_h
+#define RecoLocalCalo_HGCalRecAlgos_interface_alpaka_HGCalCalibrationParameterDeviceCollection_h
+
+#include "DataFormats/Portable/interface/alpaka/PortableCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+#include "RecoLocalCalo/HGCalRecAlgos/interface/HGCalCalibrationParameterSoA.h"
+#include "RecoLocalCalo/HGCalRecAlgos/interface/HGCalCalibrationParameterHostCollection.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  namespace hgcalrechit {
+
+    using namespace ::hgcalrechit;
+    using HGCalCalibParamDeviceCollection = PortableCollection<HGCalCalibParamSoA>;
+
+  }  // namespace hgcalrechit
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#endif  // RecoLocalCalo_HGCalRecAlgos_interface_alpaka_HGCalCalibrationParameterDeviceCollection_h
