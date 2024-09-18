b2c77d7c439ee2e3b32dcda17d843e72d9e8348c
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2024-09-18 06:57:07 +0200

    Add an ANN feature mapping & Clean up the code

--- RecoHGCal/TICL/interface/TracksterInferenceByANN.h
+++ RecoHGCal/TICL/interface/TracksterInferenceByANN.h
@@ -17,15 +17,17 @@ namespace ticl {
     static void fillPSetDescription(edm::ParameterSetDescription& iDesc);
 
   private:
+    int get_feature_order(int cell_type);
     float computeNmips(float energy, float weight, bool isSci, bool isEE, int thick);
     float computeCglob(const std::vector<float> &hit_en, const std::vector<bool> &is_ceh,
                        float elim_cee=5., float elim_ceh=5., size_t min_nhits=3, float cglob_min=0., float cglob_max=2.);
+    // source of the default values: https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L631
 
     const cms::Ort::ONNXRuntime* onnxSession_;
     const std::string modelPath_;
     const double eidMinClusterEnergy_;
-    // std::vector<float> thickness_weights_;
-    // std::vector<float> weights_;
+    std::vector<double> thickness_weights_;
+    std::vector<double> weights_;
 
     // input data
     const unsigned int MAX_ENTRIES = 50; // allowed max number of tracksters for padding input data
@@ -42,40 +44,14 @@ namespace ticl {
     const unsigned int NUM_CELL_TYPES = 11;
     const unsigned int SCINT_LAYER_THRESHOLD = 37;
 
-    std::vector<float> thickness_weights_ = {0.75, 0.76, 0.75, 0.85, 0.85, 0.84, 0.69};
-    std::vector<float> weights_ = {0.0, 9.205, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13,
-                                   11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 13.2, 13.2,
-                                   13.2, 13.2, 13.2, 13.2, 13.2, 13.2, 35.745, 59.665, 60.7, 60.7,
-                                   60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 71.89, 83.08, 83.255,
-                                   83.52, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61}; // keV
-
     //--------------------------------------------------
-    // Note
+    // NUM_CELL_TYPES & SCINT_LAYER_THRESHOLD
     //--------------------------------------------------
     // CE_E_120     = 0, CE_E_200     = 1, CE_E_300   = 2,
     // CE_H_120_F   = 3, CE_H_200_F   = 4, CE_H_300_F = 5,
     // CE_H_120_C   = 6, CE_H_200_C   = 7, CE_H_300_C = 8,
     // CE_H_SCINT_F = 9, CE_H_SCINT_C = 10, EnumSize  = 11
     // 26 CEE + 10 CEH with fine Scin. + 11 CEH with coarse Scin.
-
-    //--------------------------------------------------
-    // Description
-    //--------------------------------------------------
-    // // https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L770-782
-    // std::vector<float> default_thickness_weights = {0.75, 0.76, 0.75, 0.85, 0.85, 0.84, 0.69};
-
-    // // https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/test/hgcrechitanalyzer_cfg.py?ref_type=heads#L65
-    // // from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import calcWeights,weightsPerLayer_V16
-    // std::vector<float> default_weights = {0.0, 9.205, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13,
-    //                                        11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 13.2, 13.2,
-    //                                        13.2, 13.2, 13.2, 13.2, 13.2, 13.2, 35.745, 59.665, 60.7, 60.7,
-    //                                        60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 71.89, 83.08, 83.255,
-    //                                        83.52, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61}; // keV
-
-    // iDesc.add<std::vector<float>>("thicknessWeights", default_thickness_weights)
-    //   ->setComment("Weights for different thicknesses");
-    // iDesc.add<std::vector<float>>("weights", default_weights)
-    //   ->setComment("Weights per layer (in keV)");
   };
 }  // namespace ticl
 
--- RecoHGCal/TICL/plugins/TracksterInferenceByANN.cc
+++ RecoHGCal/TICL/plugins/TracksterInferenceByANN.cc
@@ -10,9 +10,9 @@ namespace ticl {
       modelPath_(conf.getParameter<edm::FileInPath>("onnxModelPath").fullPath()),
       eidMinClusterEnergy_(conf.getParameter<double>("eid_min_cluster_energy"))
   {
-    // // Init parameters
-    // thickness_weights_ = conf.getParameter<std::vector<float> >("thicknessWeights");
-    // weights_ = conf.getParameter<std::vector<float> >("weights");
+    // Init parameters
+    thickness_weights_ = conf.getParameter<std::vector<double> >("thicknessWeights");
+    weights_ = conf.getParameter<std::vector<double> >("weights");
 
     // Load ANN model
     static std::unique_ptr<cms::Ort::ONNXRuntime> onnxRuntimeInstance = std::make_unique<cms::Ort::ONNXRuntime>(modelPath_.c_str());
@@ -22,9 +22,7 @@ namespace ticl {
   void TracksterInferenceByANN::inputData(const std::vector<reco::CaloCluster>& layerClusters,
                                           std::vector<Trackster>& tracksters) {
     // Prepare data for inference
-    std::cout << "[INFO] TracksterInferenceByANN::inputData()" << std::endl;
-
-    // calculate the cluster energy sum & decide whether to run inference for the trackster or not
+    // Calculate the cluster energy sum & decide whether to run inference for the trackster or not
     tracksterIndices.clear(); // Clear previous indices
     for (int i = 0; i < static_cast<int>(tracksters.size()); i++) {
       float sumClusterEnergy = 0.;
@@ -37,26 +35,26 @@ namespace ticl {
           break;
         }
       }
-    }
+   }
 
-    // prepare input shapes and data for inference
+    // Prepare input shapes and data for inference
     batchSize = static_cast<int>(tracksterIndices.size());
     if (batchSize == 0) return; // Exit if no tracksters
 
-    // set constants & reset containers
+    // Set constants & reset containers
     const unsigned int lastCEE(rhtools_.lastLayerEE());
 
     std::vector<int64_t> inputShape = {MAX_ENTRIES, n_features};
     input_shapes = {inputShape};
 
     input_Data.clear();
-    input_Data.emplace_back(n_features * MAX_ENTRIES, 0); // padding for a fixed input size
+    input_Data.emplace_back(n_features * MAX_ENTRIES, 0); // Padding for a fixed input size
 
-    // loop over tracksters
+    // Loop over tracksters
     for (size_t i = 0; i < batchSize; i++) {
       const Trackster &trackster = tracksters[tracksterIndices[i]];
 
-      // initialize variables
+      // Initialize variables
       float average_eta = 0.;
       float average_phi = 0.;
       std::vector<float> v_mip_energy;
@@ -64,7 +62,7 @@ namespace ticl {
       std::vector<float> energyPerCellType(NUM_CELL_TYPES, 0.);
 
       //----------------------------------------------------------------------------------------------------
-      // calculate energy deposits in different HGCAL conpartments
+      // Calculate energy deposits in different HGCAL conpartments
       //----------------------------------------------------------------------------------------------------
       auto const &layerClusterIndices = trackster.vertices();
       for(auto const idx : layerClusterIndices) {
@@ -74,25 +72,26 @@ namespace ticl {
           bool isSci = rhtools_.isScintillator(key);
           int thick = isSci ? -1 : rhtools_.getSiThickIndex(key);
 
-          // compute and store MIP energy and CEH flag
+          // Compute and store MIP energy and CEH flag
           bool isEE(layer<=lastCEE);
           float nmips = computeNmips(lc.energy(), weights_[layer], isSci, isEE, thick);
           v_is_ceh.push_back(!isEE);
           v_mip_energy.push_back(nmips);
 
-          // compute cell type and accumulate energy
+          // Compute cell type and accumulate energy
           auto cellType = rhtools_.getCellType(key);
           if (cellType == hgcal::CE_H_SCINT) {
               cellType = (layer < SCINT_LAYER_THRESHOLD) ? CE_H_SCINT_F : CE_H_SCINT_C;
           }
-          energyPerCellType.at(cellType) += lc.energy();
+          auto feature_order = get_feature_order(cellType);
+          energyPerCellType.at(feature_order) += lc.energy();
 
-          // accumulate eta and phi
+          // Accumulate eta and phi
           average_eta += lc.eta();
           average_phi += lc.phi();
       }
 
-      // compute features
+      // Compute features
       float num_lc = static_cast<float>(layerClusterIndices.size());
       average_eta /= num_lc;
       average_phi /= num_lc;
@@ -101,7 +100,7 @@ namespace ticl {
       float fcee = (energyPerCellType[0] + energyPerCellType[1] + energyPerCellType[2]) / totalEnergy;
       float cglob = computeCglob(v_mip_energy, v_is_ceh);
 
-      // store features
+      // Store features
       auto index = i*n_features;
       input_Data[0][index] = totalEnergy;
       input_Data[0][index+1] = average_eta;
@@ -116,12 +115,11 @@ namespace ticl {
 
   void TracksterInferenceByANN::runInference(std::vector<Trackster>& tracksters) {
     // Run inference using ANN
-    std::cout << "[INFO] TracksterInferenceByANN::runInference()" << std::endl;
     if (batchSize == 0) return; // Exit if no batch
 
     // Define input and output names for inference
     std::vector<std::string> inputNames = {"x"};
-    std::vector<std::string> outNames  = {"491", "492"};
+    std::vector<std::string> outNames  = {"491", "492"}; // ratio and sigma
 
     // Run prediction
     std::vector<std::vector<float> > outputTensors;
@@ -136,35 +134,13 @@ namespace ticl {
       float raw_total_energy = input_Data[0][index];
       float regressed_energy = ratio * raw_total_energy; 
       trackster.setRegressedEnergy(regressed_energy);
-      std::cout << std::fixed << std::setprecision(2)
-                << "[result] regressed_energy = " << regressed_energy
-                << ", raw_total_energy = " << raw_total_energy
-                << ", ratio = " << ratio
-                << ", sigma = " << sigma
-                << std::endl;
-    }
-
-    // Print the input
-    std::cout << "[check] Input dimensions: " << input_Data.size() << " x " << (input_Data.empty() ? 0 : input_Data[0].size()) << std::endl;
-    for (size_t i = 0; i < input_Data.size(); ++i) {
-        std::cout << "[check] Input " << i << ": ";
-        for (size_t j = 0; j < batchSize*n_features; ++j) { std::cout << std::fixed << std::setprecision(2) << input_Data[i][j] << " "; }
-        std::cout << std::endl;
-    }
-
-    // Print the output
-    std::cout << "[check] Output dimensions: " << outputTensors.size() << " x " << (outputTensors.empty() ? 0 : outputTensors[0].size()) << std::endl;
-    for (size_t i = 0; i < outputTensors.size(); ++i) {
-        std::cout << "[check] Output " << i << ": ";
-        for (size_t j = 0; j < batchSize; ++j) { std::cout << std::fixed << std::setprecision(2) << outputTensors[i][j] << " "; }
-        std::cout << std::endl;
     }
   } // end of runInference
 
   float TracksterInferenceByANN::computeNmips(float energy, float weight, bool isSci, bool isEE, int thick) {
-    // conver energy back to nmips
-    // Erec = weight * nmips / thick_weight <=> nmips = Erec * thick_weight / weight; // weights are in keV, energy in GeV
-    // https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L770-782
+    // Conver energy back to nmips
+    // Erec = weight * nmips / thick_weight <=> nmips = Erec * thick_weight / weightm where weights are in keV, energy in GeV
+    // Reference: https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L770-782
     float mipSF(1./(weight*1e-3));
     if(!isSci) {
       size_t idx(isEE ? 0 : 3);
@@ -181,12 +157,14 @@ namespace ticl {
 
   float TracksterInferenceByANN::computeCglob(const std::vector<float> &hit_en, const std::vector<bool> &is_ceh,
                                               float elim_cee, float elim_ceh, size_t min_nhits, float cglob_min, float cglob_max) {
+    // Compute a variable value associated with global correlation
+    // Reference: https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L631
     size_t nhits(hit_en.size());
     if(nhits<min_nhits) return -1.;
 
     float mean_en = std::accumulate(hit_en.begin(),hit_en.end(),0.0) / nhits;
 
-    //count hits below average or below elim~5MIP
+    // Count hits below average or below elim~5MIP
     size_t nbelow_elim(0);
     size_t nbelow_mean(0);
     for(size_t i=0; i<nhits; i++) {
@@ -196,18 +174,57 @@ namespace ticl {
     }
     if(nbelow_mean==0) return -1.;
 
-    //cglob ratio
+    // cglob ratio
     float cglob=float(nbelow_elim)/float(nbelow_mean);
     cglob=std::min(cglob_max,std::max(cglob_min,cglob));
 
     return cglob;
   }
 
-  // Method to fill parameter set description for configuration
+  int TracksterInferenceByANN::get_feature_order(int cell_type) {
+    // A mapping between the cell types in rhtools & the feature orders in ANN models
+    switch (cell_type) {
+        case 0: return 0;  // CE_E_120 -> sumen_CEESi100
+        case 1: return 1;  // CE_E_200 -> sumen_CEESi200
+        case 2: return 2;  // CE_E_300 -> sumen_CEESi300
+        case 3: return 6;  // CE_H_120_F -> sumen_CEHfineSi100
+        case 4: return 7;  // CE_H_200_F -> sumen_CEHfineSi200
+        case 5: return 8;  // CE_H_300_F -> sumen_CEHfineSi300
+        case 6: return 3;  // CE_H_120_C -> sumen_CEHcoarseSi100
+        case 7: return 4;  // CE_H_200_C -> sumen_CEHcoarseSi200
+        case 8: return 5;  // CE_H_300_C -> sumen_CEHcoarseSi300
+        case 9: return 10; // CE_H_SCINT_F -> sumen_CEHScifine
+        case 10: return 9; // CE_H_SCINT_C -> sumen_CEHScicoarse
+        default:
+            throw std::invalid_argument("Invalid cell type");
+    }
+  }
+
   void TracksterInferenceByANN::fillPSetDescription(edm::ParameterSetDescription& iDesc) {
+    // Method to fill parameter set description for configuration
     iDesc.add<int>("algo_verbosity", 0);
-    iDesc.add<edm::FileInPath>("onnxModelPath", edm::FileInPath("RecoHGCal/TICL/data/model.onnx")) // semiparametric_model_lc_ca_r02_2024Apr29.onnx
+
+    // Path to an onnx model, tk = trackster
+    iDesc.add<edm::FileInPath>("onnxModelPath", edm::FileInPath("RecoHGCal/TICL/data/ticlv5/onnx_models/hadron_energy_regression/semiparametric_model_tk_pca_2024Apr29.onnx"))
       ->setComment("Path to PyTorch ANN model in ONNX format");
+
+    // Energy threshold to select a trackster
     iDesc.add<double>("eid_min_cluster_energy", 1.0);
+
+    // Reference: https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/src/HGCRecHitAnalyzer.cc?ref_type=heads#L770-782
+    std::vector<double> default_thickness_weights = {0.75, 0.76, 0.75, 0.85, 0.85, 0.84, 0.69};
+
+    // Reference: https://gitlab.cern.ch/psilva/hgcalhadronicenergyscalestudies/-/blob/master/test/hgcrechitanalyzer_cfg.py?ref_type=heads#L65
+    // from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import calcWeights,weightsPerLayer_V16
+    std::vector<double> default_weights = {0.0, 9.205, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13,
+                                           11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 11.13, 13.2, 13.2,
+                                           13.2, 13.2, 13.2, 13.2, 13.2, 13.2, 35.745, 59.665, 60.7, 60.7,
+                                           60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 60.7, 71.89, 83.08, 83.255,
+                                           83.52, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61, 83.61}; // keV
+
+    iDesc.add<std::vector<double>>("thicknessWeights", default_thickness_weights)
+      ->setComment("Weights for different thicknesses");
+    iDesc.add<std::vector<double>>("weights", default_weights)
+      ->setComment("Weights per layer (in keV)");
   }
 }  // namespace ticl
